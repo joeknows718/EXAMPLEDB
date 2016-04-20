dfb673fa5fd5a036c28276e19e36b2dde3a2b146
JoeC718 <joe.carrano@colorofchange.org>
2016-04-20 16:58:03 -0400

    dash fix

--- pro_db/templatetags/custom_filters.py
+++ pro_db/templatetags/custom_filters.py
@@ -14,15 +14,15 @@ def sort_by(queryset, order):
 
 @register.filter
 def filter_date_and_sort_general(queryset, order):
-	return queryset.filter(general_election_date__gte=date.today()).order_by('state__state_name',order).distinct()
+	return queryset.filter(general_election_date__gte=date.today()).order_by(order).distinct('general_election_date')
 
 
 @register.filter
 def filter_date_and_sort_primary(queryset, order):
-	return queryset.filter(primary_election_date__gte=date.today()).order_by('state__state_name',order).distinct()
+	return queryset.filter(primary_election_date__gte=date.today()).order_by(order).distinct()
 
 
 
 @register.filter
 def filter_date_and_sort_filing(queryset, order):
-	return queryset.filter(next_filing_date__gte=date.today()).order_by('state__state_name',order).distinct()
+	return queryset.filter(next_filing_date__gte=date.today()).order_by(order).distinct()
