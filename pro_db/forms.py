504b719ee2c8a5c779eb820829a6d9bb6c29d529
JoeC718 <joe.carrano@colorofchange.org>
2016-04-21 15:00:53 -0400

    gender/race

--- pro_db/forms.py
+++ pro_db/forms.py
@@ -58,9 +58,18 @@ class RegisterForm(UserCreationForm):
 		model = User
 		fields = ('username', 'email', 'first_name', 'last_name', 'organization', 'password1', 'password2')
 
+
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		username = self.cleaned_data.get('username')
+		if email and User.objects.filter(email=email).exclude(username=username).count():
+			raise forms.ValidationError(u'Email addresses must be unique.')
+		return email
+
 	def save(self, commit=True):
 		user = super(UserCreationForm, self).save(commit=False)
 		user.email = self.cleaned_data['email']
+		self.clean_email() 
 		user.first_name = self.cleaned_data['first_name']
 		user.last_name = self.cleaned_data['last_name']
 		user.organization = self.cleaned_data['organization']
