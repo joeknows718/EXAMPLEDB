efe8a71ea8652d8d2bf3b12e2d8522afa1b50db1
JoeC718 <joe.carrano@colorofchange.org>
2016-06-14 17:05:37 -0400

    test

--- pro_db/views.py
+++ pro_db/views.py
@@ -382,17 +382,17 @@ class CandidateReportGen(LoginRequiredMixin,
 		else:
 			queryset = queryset.filter(election__state__state_name=state_param)
 
-		gender_param = form.cleaned_data['gender']
-		if gender_param is None or gender_param < 1:
-			pass
-		else:
-			queryset = queryset.filter(gender=str(gender_param))
-
-		#race_param = form.cleaned_data['race']
-		#if race_param is None or race_param < 1:
+		#gender_param = form.cleaned_data['gender']
+		#if gender_param is None or gender_param < 1:
 		#	pass
 		#else:
-		#	queryset = queryset.filter(race=str(race_param))
+		#	queryset = queryset.filter(gender=str(gender_param))
+
+		race_param = form.cleaned_data['race']
+		if race_param is None or race_param < 1:
+			pass
+		else:
+			queryset = queryset.filter(race=str(race_param))
 
 		
 
